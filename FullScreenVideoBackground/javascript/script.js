c7c6eae8fbbf0a98fb8582b3cb06f7727de3940d
Chaitanya <chaitu.vsh@gmail.com>
2021-04-29 23:57:15 +0530

    Added menu button in the top right and JS handler

--- FullScreenVideoBackground/javascript/script.js
+++ FullScreenVideoBackground/javascript/script.js
@@ -1,3 +1,12 @@
+// ################################################
+// Do not modify the below block
 window.addEventListener("DOMContentLoaded", () => {
     console.log("Up and running...");
+})
+// ################################################
+// Make your code changes from the below
+
+let menuBtn = document.getElementById("menuBtn");
+menuBtn.addEventListener("click", () => {
+    alert("Promoted")
 })

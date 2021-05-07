77b0a7834b9e6e8092b305cb4a958e203015e8ae
Chaitanya <chaitu.vsh@gmail.com>
2021-05-07 08:01:59 +0530

    Added styles for the right side menu

--- FullScreenVideoBackgroundWithSideMenu/javascript/script.js
+++ FullScreenVideoBackgroundWithSideMenu/javascript/script.js
@@ -4,9 +4,10 @@ let menuBtn = document.getElementById("menuBtn");
 let section = document.getElementById("sectionMenu");
 let menuOpenIcon = document.getElementById("open");
 let menuCloseIcon = document.getElementById("close");
+// let menuItems = document.getElementById("menuItems");
 
 menuBtn.addEventListener("click", () => {
-    menuOpenIcon.classList.toggle("hide")
-    menuCloseIcon.classList.toggle("hide")
-    section.classList.toggle("active")
+    menuOpenIcon.classList.toggle("hide");
+    menuCloseIcon.classList.toggle("hide");
+    section.classList.toggle("active");
 });

12d4c96a7cfa9b7a0adc2232b0e125b9ca550b9b
Chaitanya <chaitu.vsh@gmail.com>
2021-05-07 07:41:45 +0530

    [CSS][ULTRA PRO PRO PRO]: Added styles for Menu and Showcase transistions

--- FullScreenVideoBackgroundWithSideMenu/javascript/script.js
+++ FullScreenVideoBackgroundWithSideMenu/javascript/script.js
@@ -1,7 +1,12 @@
 console.log("Up and running...!")
 
-let menu = document.getElementById("menuBtn")
+let menuBtn = document.getElementById("menuBtn");
+let section = document.getElementById("sectionMenu");
+let menuOpenIcon = document.getElementById("open");
+let menuCloseIcon = document.getElementById("close");
 
-menu.addEventListener("click", () => {
-    
+menuBtn.addEventListener("click", () => {
+    menuOpenIcon.classList.toggle("hide")
+    menuCloseIcon.classList.toggle("hide")
+    section.classList.toggle("active")
 });

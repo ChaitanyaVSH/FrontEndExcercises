5ee289ea55884419c55f969085fe021c9ef399f0
Chaitanya <chaitu.vsh@gmail.com>
2021-06-13 22:23:03 +0530

    [CSS][ULTRA PRO]: Implemented right side menu

--- EasyLearnClone1/javascript/scripts.js
+++ EasyLearnClone1/javascript/scripts.js
@@ -1,1 +1,8 @@
 console.log("Up and running...")
+
+let menuBtn = document.querySelector(".menuImg");
+let menu = document.querySelector(".menu")
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("moveright");
+})
